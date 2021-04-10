bd9a902b17c6b5528a97320f698145a172408d05
Saif Mustafa <Saif.Mustafa94@gmail.com>
2021-04-10 02:58:46 -0700

    Updated upload message / added launch localhost and changed way initial parse db works
    Updated async function to resolve parseDB promise only when all DBs have been parsed during initial run- results in slower update of small files, but ensures all files are fully available before system launch. - does not trigger on subsequent partial db updates
    
    Updated upload return message to include more details
    
    Added feature to launch website browser link once all dbs have been parsed

--- routes/functions/startup.js
+++ routes/functions/startup.js
@@ -1,94 +1,76 @@
 
-    const fs = require('fs');
+const fs = require('fs');
 
-    const uploads = "./public/uploads";
-    let folderPath = [];
-   
-    const parseDB = require('./parseDB');
-    const pullFolderAndFileList = require('./pullFolderAndFileList');
+const uploads = "./public/uploads";
+let folderPath = [];
 
-    let dataObject = undefined;
-    dataObject = {};
+const parseDB = require('./parseDB');
+const pullFolderAndFileList = require('./pullFolderAndFileList');
 
-
-    function startup(){
-
-    return new Promise(function(resolve, reject) {
-    pullFolderAndFileList.pullFolderAndFileList().then(
-        
-        async function(response) {
-            folderPath = response;
+let dataObject = undefined;
+dataObject = {};
 
 
-            // //for each file in folderpath call database parser
-            // folderPath.forEach(element => {
-            //     console.log("Startup: Found directories - index term " ,element[2])
-            //     let directoryRoute = element[0];
-            //     let fileRoute = element[1];
-            //     let directoryNameForURL = element[2];
-            //     let keyword = element[3];
-            //     // console.log("startup - fileRoute",fileRoute,"\n","startup- keyword",keyword)
+function startup(firstLaunch) {
+    // console.log(firstLaunch)
+    return new Promise(function (resolve, reject) {
+        pullFolderAndFileList.pullFolderAndFileList().then(
 
-            //     //second promise function, takes our list of directories and keywords and parses them into DataObject
-            //     parseDB.parseDB(fileRoute,keyword,directoryNameForURL).then(
-            //         function(response2){
-            //             //IF unable to parse file return error for given file keyword
-            //             if(response2["error"] == true){
-            //                 console.log("db " + directoryNameForURL + " is not valid, please fix the csv and reupload")
-            //                 directoryNameForURL = directoryNameForURL
-            //                 dataObject[directoryNameForURL] = response2;
-            //                 dataObject[directoryNameForURL]["message"] = "db " + directoryNameForURL + " is not valid, please fix the csv and reupload";
-            //                 resolve(dataObject)
-            //             }else{
-            //             //assign returned data set to dataObject
-            //             dataObject[directoryNameForURL] = response2;
-            //             resolve(dataObject)
-            //             }
-            //         }
-            //     )
-            // });
+            async function (response) {
+                folderPath = response;
+                const len = folderPath.length
 
-            for (const element of folderPath) {
-                console.log("Startup: Found directories - index term " ,element[2])
-                let directoryRoute = element[0];
-                let fileRoute = element[1];
-                let directoryNameForURL = element[2];
-                let keyword = element[3];
+                for (const [index, element] of folderPath.entries()) {
+                    console.log("Startup: Found directories - index term ", element[2])
+                    let directoryRoute = element[0];
+                    let fileRoute = element[1];
+                    let directoryNameForURL = element[2];
+                    let keyword = element[3];
+                    console.log("parsing database",index+1," of ", len)
 
 
-                const contents = await  parseDB.parseDB(fileRoute,keyword,directoryNameForURL).then(
-                    function(response2){
-                        //IF unable to parse file return error for given file keyword
-                        if(response2["error"] == true){
-                            console.log("db " + directoryNameForURL + " is not valid, please fix the csv and reupload")
-                            directoryNameForURL = directoryNameForURL
-                            dataObject[directoryNameForURL] = response2;
-                            dataObject[directoryNameForURL]["message"] = "db " + directoryNameForURL + " is not valid, please fix the csv and reupload";
-                            resolve(dataObject)
-                        }else{
-                        //assign returned data set to dataObject
-                        dataObject[directoryNameForURL] = response2;
-                        resolve(dataObject)
+                    const contents = await parseDB.parseDB(fileRoute, keyword, directoryNameForURL).then(
+                        function (response2) {
+                            //IF unable to parse file return error for given file keyword
+                            if (response2["error"] == true) {
+                                console.log("db " + directoryNameForURL + " is not valid, please fix the csv and reupload")
+                                directoryNameForURL = directoryNameForURL
+                                dataObject[directoryNameForURL] = response2;
+                                dataObject[directoryNameForURL]["message"] = "db " + directoryNameForURL + " is not valid, please fix the csv and reupload";
+                                if(firstLaunch){
+                                if(index >= len -1){
+                                    resolve(dataObject)
+                                }}else{
+                                    resolve(dataObject)
+                                }
+                            } else {
+                                //assign returned data set to dataObject
+                                dataObject[directoryNameForURL] = response2;
+                                if(firstLaunch){
+                                    if(index >= len -1){
+                                        resolve(dataObject)
+                                    }}else{
+                                        resolve(dataObject)
+                                    }
+                            }
                         }
-                    }
-                )
-              }
+                    )
+                }
 
 
+            }, function (error) { // if error in pullFolderAndFileList
+                console.error("Failed during pullFolderAndFileList!", error);
+            }
+            )
+
+    })
 
 
-        }, function(error) {
-        console.error("Failed!", error);
-      })
-
-   })
-
-        
 
 
 }
-  
-  
-  
-  // add the code below 
-  module.exports = {startup }
+
+
+
+// add the code below 
+module.exports = { startup }
